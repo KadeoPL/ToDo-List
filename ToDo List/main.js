7b0f6f7d97685bb55638b7dc5f2fd663b3f9bda7
KadeoPL <stopkakrzysztof1@gmail.com>
2024-03-11 20:25:29 +0100

    add validation to input box, add EventListeners

--- ToDo List/main.js
+++ ToDo List/main.js
@@ -1,1 +1,21 @@
 import './style.css'
+
+let inputValue = document.getElementById('input-box');
+const inputBtn = document.getElementById('input-btn');
+
+inputBtn.addEventListener('click', () => {
+  addTask(inputValue.value);
+})
+
+inputValue.addEventListener('keypress', () => {
+  addTask(inputValue.value);
+})
+
+function addTask(text){
+  console.log(text);
+  if(!inputValue.value) {
+    console.log('Pusty formularz');
+  } else {
+    inputValue.value = '';
+  }
+}
