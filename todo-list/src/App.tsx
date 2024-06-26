98ecec5d396bbd37ac905ae092715b66c59c7768
Krzysztof Stopka <stopkakrzysztof1@gmail.com>
2024-06-26 22:56:05 +0200

    addTask function

--- todo-list/src/App.tsx
+++ todo-list/src/App.tsx
@@ -1,9 +1,23 @@
+import React, { useState } from "react";
 
 function App() {
+  const [text, setText] = useState('');
+
+  const addTask = ((text:string) => {
+    console.log(text)
+    setText('');
+  })
 
   return (
-   
-  )
+    <>
+      <input 
+        type="text" 
+        value={text} 
+        onChange={(e: React.ChangeEvent<HTMLInputElement>) => setText(e.target.value)} 
+      />
+      <button onClick={() => addTask(text)}>Ok</button>
+    </>
+  );
 }
 
-export default App
+export default App;
